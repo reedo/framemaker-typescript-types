084512a29f774a274a9fc614f9e672ebec49105a
reedo <info@reederror.com>
2019-07-22 21:03:21 +0100

    Made comments markdown complient

--- extendscript/$.d.ts
+++ extendscript/$.d.ts
@@ -19,13 +19,16 @@ interface $ {
 
 	/**
    * The Internet name of the application’s default character encoding, such as “CP1252” or “X-SHIFT-JIS”. Valid values are implementation- and OS-dependent.
+	 * 
    * Set to change the default encoding for the application. The returned value can differ from the value set. In Windows, for example, if set to “x-latin1”, the returned value is the synonymous “ISO-8859-1”.
    */
 	appEncoding: string;
 
 	/**
    * Executes a breakpoint at the current position.
+	 * 
    * If no condition is needed, it is recommended that you use the JavaScript debugger statement in the script, rather than this method.
+	 * 
    * @param condition A string containing a JavaScript statement to be used as a condition. If the statement evaluates to true or nonzero when this point is reached, execution stops.
    */
 	bp(condition?: string): void;
@@ -38,6 +41,7 @@ interface $ {
 
 	/**
    * Invokes the platform-specific color selection dialog, and returns the selected color as a hexadecimal RGB value: 0xRRGGBB.
+	 * 
    * @param name The color to be preselected in the dialog, as a hexadecimal RGB value (0xRRGGBB), or -1 for the platform default.
    */
 	colorPicker(name: number): number;
@@ -53,12 +57,14 @@ interface $ {
 
 	/**
    * The most recent run-time error information, contained in a JavaScript Error object.
+	 * 
    * Assigning error text to this property generates a run-time error; however, the preferred way to generate a run-time error is to throw an Error object.
    */
 	error: Error | string;
 
 	/**
    * Loads a JavaScript script file from disk, evaluates it, and returns the result of evaluation.
+	 * 
    * @param path The name and location of the file.
    * @param timeout A number of milliseconds to wait before returning undefined, if the script cannot be evaluated. Default is 10000 milliseconds.
    */
@@ -69,13 +75,18 @@ interface $ {
 
 	/**
    * Gets or sets low-level debug output flags.
+	 * 
    * A logical AND of bit flag values:
-   * 0x0002 (2): Displays each line with its line number as it is executed.
-   * 0x0040 (64): Enables excessive garbage collection. Usually, garbage collection starts when the number of objects has increased by a certain amount since the last garbage collection. This flag causes ExtendScript to garbage collect after almost every statement. This impairs performance severely, but is useful when you suspect that an object gets released too soon.
-   * 0x0080 (128): Displays all calls with their arguments and the return value.
-   * 0x0100 (256): Enables extended error handling (see strict).
-   * 0x0200 (512): Enables the localization feature of the toString method. Equivalent to the localize property.
-   * NOTE: Other bit values are not public and should not be used.
+	 * 
+	 * Hex | Decimal | Description 
+	 * :---: | :---: | :---
+	 * 0x0002 | 2 | Displays each line with its line number as it is executed.
+   * 0x0040 | 64 | Enables excessive garbage collection. Usually, garbage collection starts when the number of objects has increased by a certain amount since the last garbage collection. This flag causes ExtendScript to garbage collect after almost every statement. This impairs performance severely, but is useful when you suspect that an object gets released too soon.
+   * 0x0080 | 128 | Displays all calls with their arguments and the return value.
+   * 0x0100 | 256 | Enables extended error handling (see strict).
+   * 0x0200 | 512 | Enables the localization feature of the toString method. Equivalent to the localize property.
+	 * 
+   * __NOTE__: Other bit values are not public and should not be used.
    */
 	flags: number;
 
@@ -84,6 +95,7 @@ interface $ {
 
 	/**
    * Retrieves the value of the specified environment variable, or null if no such variable is defined.
+	 * 
    * @param envname The name of the environment variable.
    */
 	getenv(envname: string): string | null;
@@ -105,6 +117,7 @@ interface $ {
 
 	/**
 	 * Gets or sets the current locale. The string contains five characters in the form LL_RR, where LL is an ISO 639 language specifier, and RR is an ISO 3166 region specifier.
+	 * 
 	 * Initially, this is the value that the application or the platform returns for the current user. You can set it to temporarily change the locale for testing. To return to the application or platform setting, set to undefined, null, or the empty string.
 	 */
 	locale: number;
@@ -123,6 +136,7 @@ interface $ {
 
 	/**
    * Sets the value of the specified environment variable, if no such variable is defined.
+	 * 
    * @param envname The name of the environment variable.
    * @param value The new value, a string.
    */
@@ -130,7 +144,9 @@ interface $ {
 
 	/**
    * Suspends the calling thread for the given number of milliseconds.
+	 * 
    * During a sleep period, checks at 100 millisecond intervals to see whether the sleep should be terminated. This can happen if there is a break request, or if the script timeout has expired.
+	 * 
    * @param milliseconds The number of milliseconds to wait.
    */
 	sleep(milliseconds: number): void;
@@ -149,6 +165,7 @@ interface $ {
 
 	/**
    * Writes the specified text to the JavaScript Console.
+	 * 
    * @param text A text string to write.
    * @param rest Any number of additional strings. All are concatenated to form a single string.
    */
@@ -156,6 +173,7 @@ interface $ {
 
 	/**
 	 * Writes the specified text to the JavaScript Console and appends a linefeed sequence.
+	 * 
 	 * @param text A text string to write.
 	 * @param rest Any number of additional strings. All are concatenated to form a single string.
 	 */
