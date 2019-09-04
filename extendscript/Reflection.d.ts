a17f91562fe0da8760f5406d382bd8728c8b6dda
reedo <info@reederror.com>
2019-09-04 01:31:19 +0100

    added Reflection and ReflectionInfo

---
+++ extendscript/Reflection.d.ts
@@ -0,0 +1,48 @@
+// Type definitions for the Reflection ExtendScript interface
+// Definitions by Dan Reed <https://danreed.dev>
+
+/**
+ * Every object has a reflect property that returns a reflection object that reports the contents of the object.
+ * 
+ * You can, for example, show the values of all the properties of an object with code like this:
+ * 
+ * @example
+ * var f = new File ("myfile");
+ * var props = f.reflect.properties;
+ * for (var i = 0; i < props.length; i++) {
+ *   $.writeln(’this property ’ + props[i].name + ’ is ’ + f[props[i].name]);
+ * }
+ */
+interface Reflection {
+
+	/** Short text describing the reflected object, or `undefined` if no description is available. */
+	description: string | undefined;
+
+	/**
+	 * Use this method to get information about dynamic properties that have not yet been accessed, but that are known to exist.
+	 * 
+	 * @param name The property for which to retrieve information.
+	 * 
+	 * @returns The `ReflectionInfo` object for the named property of the reflected object, or `null` if no such property exists.
+	 */
+	find(name: string): ReflectionInfo | null;
+
+	/** Longer text describing the reflected object more completely, or undefined if no description is available. */
+	help: string;
+
+	/** An Array of `ReflectionInfo` objects containing all methods of the reflected object, defined in the class or in the specific instance. */
+	methods: ReflectionInfo[];
+
+	/** The class name of the reflected object. */
+	name: string;
+
+	/**
+	 * An Array of ReflectionInfo objects containing all properties of the reflected object, defined in the class or in the specific instance.
+	 * 
+	 * For objects with dynamic properties (defined at runtime) the list contains only those dynamic properties that have already been accessed by the script.
+	 * 
+	 * For example, in an object wrapping an HTML tag, the names of the HTML attributes are determined at run time.
+	*/
+	properties: ReflectionInfo[];
+
+}
---
+++ extendscript/ReflectionInfo.d.ts
@@ -0,0 +1,72 @@
+// Type definitions for the ReflectionInfo ExtendScript interface
+// Definitions by Dan Reed <https://danreed.dev>
+
+/**
+ * This object contains information about a property, a method, or a method argument.
+ * 
+ * You can access `ReflectionInfo` objects in a `Reflection` object’s properties and methods arrays, by name or index.
+ * 
+ * You can access the `ReflectionInfo` objects for the arguments of a method in the arguments array of the `ReflectionInfo` object for the method, by index.
+ * 
+ * @example
+ * obj = new String ('hi');
+ * obj.reflect.methods[0];
+ * obj.reflect.methods['indexOf'];
+ * 
+ * @example
+ * obj.reflect.methods['indexOf'].arguments[0];
+ * obj.reflect.methods.indexOf.arguments[0];
+ */
+interface ReflectionInfo {
+
+	/** For a reflected method, an array of `ReflectionInfo` objects describing each method argument. */
+	arguments: ReflectionInfo[];
+
+	/**
+	 * The data type of the reflected element. One of:
+	 * 
+	 * * boolean
+	 * * number
+	 * * string
+	 * * _Classname_ - The class name of an object.
+	 * * \* - Any type. This is the default.
+	 * * null
+	 * * undefined - Return data type for a function that does not return any value.
+	 * * unknown
+	 * 
+	 * __NOTE__: Class names start with a capital letter. Thus, the value string stands for a javascript `string`, while `String` is a javascript `String` wrapper object.
+	 */
+	dataType: string;
+
+	/** The default value for a reflected property or method argument, or `undefined` if there is no default value, if the property is undefined, or if the element is a method. */
+	defaultValue: any;
+
+	/** Short text describing the reflected object, or `undefined` if no description is available. */
+	description: string | undefined;
+
+	/** Longer text describing the reflected object more completely, or `undefined` if no description is available. */
+	help: string | undefined;
+
+	/** When true, the reflected property or method returns a collection; otherwise, false. */
+	isCollection: boolean;
+
+	/** The maximum numeric value for the reflected element, or `undefined` if there is no maximum or if the element is a method. */
+	max: number | undefined;
+
+	/** The minimum numeric value for the reflected element, or `undefined` if there is no minimum or if the element is a method. */
+	min: number | undefined;
+
+	/** The name of the reflected element. A string, or a number for an array index. */
+	name: string | number;
+
+	/**
+	 * The type of the reflected element. One of:
+	 * 
+	 * * `readonly` - A Read only property.
+	 * * `readwrite` - A read-write property.
+	 * * `createonly` - A property that is valid only during creation of an object.
+	 * * `method` - A method.
+	 */
+	type: 'readonly' | 'readwrite' | 'createonly' | 'method';
+
+}
