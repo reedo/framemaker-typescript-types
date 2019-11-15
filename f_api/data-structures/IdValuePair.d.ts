e60bcb08f60268409287491f953faae6491ea1fb
reedo <info@reederror.com>
2019-11-15 02:04:21 +0000

    Added IdValuePair

---
+++ f_api/data-structures/IdValuePair.d.ts
@@ -0,0 +1,21 @@
+// Type definitions for the IdValuePair FMObject
+// Definitions by: Dan Reed <http://danreed.dev>
+
+/** A single integer or string value. */
+interface IdValuePair {
+
+	/** The ID. */
+	id: number;
+
+	/** The value. */
+	value: Value;
+
+}
+
+interface IdValuePairConstructor {
+	new(): IdValuePair;
+	new(id: number, value: Value): IdValuePair;
+	readonly prototype: IdValuePair;
+}
+
+declare var IdValuePair: IdValuePairConstructor;
