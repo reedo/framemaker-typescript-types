e9cc6cc5a70365bce618bb58d691fd82a80fb755
reedo <info@reederror.com>
2019-11-15 01:07:53 +0000

    Added ElementLoc

---
+++ f_api/data-structures/ElementLoc.d.ts
@@ -0,0 +1,23 @@
+// Type definitions for the ElementLoc FMObject
+// Definitions by: Dan Reed <http://danreed.dev>
+
+/** ElementLoc specifies a location within the structure of a document or book. */
+interface ElementLoc {
+
+	/** The parent element. */
+	parent: Element;
+
+	/** The child element. */
+	child: Element;
+
+	/** Offset within the parent or child element. */
+	offset: number;
+}
+
+interface ElementLocConstructor {
+	new(): ElementLoc;
+	new(parent: Element, child: Element, offset: number): ElementLoc;
+	readonly prototype: ElementLoc;
+}
+
+declare var ElementLoc: ElementLocConstructor;
