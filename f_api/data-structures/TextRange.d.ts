d931bf108ef8e901a74162672a5284ddffdd71b7
reedo <info@reederror.com>
2019-11-15 00:29:01 +0000

    Added TextRange

---
+++ f_api/data-structures/TextRange.d.ts
@@ -0,0 +1,29 @@
+// Type definitions for the TextRange FMObject
+// Definitions by: Dan Reed <http://danreed.dev>
+
+/**
+ * TextRange specifies a text range.
+ * 
+ * A text range can span paragraphs. However, it cannot span graphic text lines or flows.
+ * 
+ * __NOTE__: beg.offset and end.offset fields of aTextRange object can specify offsets relative to the beginning and end of an object. In addition, they can use the special value `Constants.FV_OBJ_END_OFFSET` (0x50000000).
+ * 
+ * __NOTE__: `Constants.FV_OBJ_END_OFFSET` (0x50000000) specifies the offset of the last character in the object containing the text range.
+ */
+interface TextRange {
+
+	/** The beginning of the text range. */
+	beg: TextLoc;
+
+	/** The end of the text range. */
+	end: TextLoc;
+}
+
+interface TextRangeConstructor {
+	new(): TextRange;
+	new(beg: TextLoc, end: TextLoc): TextRange;
+	readonly prototype: TextRange;
+
+}
+
+declare var TextRange: TextRangeConstructor;
