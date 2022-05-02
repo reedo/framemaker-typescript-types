408ab6f7e6a057bd434897c65309c6a1debad0ef
ReedError <info@reederror.com>
2022-05-02 23:44:31 +0100

    Done (#130)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

---
+++ f_api/objects/Element.d.ts
@@ -0,0 +1,130 @@
+// Type definitions for FrameMaker Element object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface Element extends FMUniqueObject {
+	AllowAsSpecialCase: boolean;
+
+	AttrDisplay: number;
+
+	AttributeValueInvalid: boolean;
+
+	Attributes: Attributes;
+
+	AttributesEx: AttributesEx;
+
+	readonly BookComponent: BookComponent;
+
+	readonly BookComponentMissing: boolean;
+
+	readonly ContentIsLooselyValid: boolean;
+
+	readonly ContentIsStrictlyValid: boolean;
+
+	readonly ContentMustBeEmpty: boolean;
+
+	readonly ContentNeededAtBegin: boolean;
+
+	readonly ContentNeededAtEnd: boolean;
+
+	readonly ContextLabel: string;
+
+	Direction: number;
+
+	ElementDef: ElementDef;
+
+	ElementIsCollapsed: boolean;
+
+	readonly ElementIsExcludedInContext: boolean;
+
+	readonly ElementIsInvalidInParent: boolean;
+
+	readonly ElementIsInvalidInPosition: boolean;
+
+	readonly ElementIsTopicGroup: boolean;
+
+	readonly ElementIsTopicHead: boolean;
+
+	readonly ElementIsTopicRef: boolean;
+
+	readonly ElementIsUndefined: boolean;
+
+	ElementMarkedForNamedDestination: boolean;
+
+	readonly ElementType: ElementType;
+
+	readonly ErrorInBookComponent: boolean;
+
+	readonly FirstChildElement: Element;
+
+	readonly FormatOverride: boolean;
+
+	readonly HoleBeforeElement: boolean;
+
+	readonly InvalidHighestLevel: boolean;
+
+	readonly LastChildElement: Element;
+
+	readonly MatchingFirstPgfClauses: FmtRuleClause[];
+
+	readonly MatchingLastPgfClauses: FmtRuleClause[];
+
+	readonly MatchingObjectClauses: FmtRuleClause[];
+
+	readonly MatchingPrefixClauses: FmtRuleClause[];
+
+	readonly MatchingSuffixClauses: FmtRuleClause[];
+
+	readonly MatchingTextClauses: FmtRuleClause[];
+
+	Namespace: Strings;
+
+	readonly NamespaceScope: Element;
+
+	readonly NextDITAConrefElementInDoc: Element;
+
+	readonly NextDITALinkElementInDoc: Element;
+
+	readonly NextDITATopicrefElementInDoc: Element;
+
+	readonly NextDITATopicsetrefElementInDoc: Element;
+
+	readonly NextDITAXrefElementInDoc: Element;
+
+	readonly NextElementDFS: Element;
+
+	readonly NextInvalidElement: Element;
+
+	readonly NextSiblingElement: Element;
+
+	readonly NumNamespaces: number;
+
+	readonly Object: FMObject;
+
+	readonly ParentElement: Element;
+
+	readonly PrevElementDFS: Element;
+
+	readonly PrevSiblingElement: Element;
+
+	ResolvedDirection: number;
+
+	readonly TextIsInvalidInElement: boolean;
+
+	readonly TextRange: TextRange;
+
+	readonly TopicRefAbsoluteFilePath: string;
+
+	UserString: string;
+
+	readonly ValidationFlags: number;
+
+	Delete(): FrameErrorCode;
+
+	GetAttributes(): Attributes;
+
+	GetText(flags: number): TextItems;
+
+	GetText2(flags: number, flags2: number): TextItems;
+
+	UpdateDITAReference(): FrameErrorCode;
+}
