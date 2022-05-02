942599eda49827b0ca784a0c280d9a24605c13b8
dan.reed <dan.reed@vistair.com>
2022-05-03 00:57:47 +0100

    Done

---
+++ f_api/objects/FmtRuleClause.d.ts
@@ -0,0 +1,36 @@
+// Type definitions for FrameMaker FmtRuleClause object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface FmtRuleClause extends FMObject {
+	ContextLabel: string;
+
+	ElemPrefixSuffix: string;
+
+	readonly FmtChangeList: FmtChangeList;
+
+	FmtChangeListTag: string;
+
+	readonly FmtRule: FmtRule;
+
+	FormatTag: string;
+
+	IsTextRange: boolean;
+
+	readonly RuleClauseType:
+		| FV_RC_TAG
+		| FV_RC_SUB_FMTRULE
+		| FV_RC_CHANGELIST
+		| FV_RC_CHANGELIST_TAG;
+
+	Specification: string;
+
+	readonly SpecificationForCSS: Strings;
+
+	readonly SubFmtRule: FmtRule;
+
+	Delete(): FrameErrorCode;
+
+	NewFmtChangeList(): FmtChangeList;
+
+	NewSubFmtRule(): FmtRule;
+}
