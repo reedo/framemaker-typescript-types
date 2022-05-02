acf1afff56fe3ad5bc3696757bdb6f78b030f76c
ReedError <info@reederror.com>
2022-05-02 23:55:32 +0100

    Done (#131)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

--- makeTypedefFile.js
+++ makeTypedefFile.js
@@ -2,7 +2,7 @@
 	var BASE_DIR =
 		'C:/Vistair/FMConfig/FM16/ExtendScripts/types/framemaker-typescript-types/f_api/objects/';
 
-	var objectName = 'Element';
+	var objectName = 'Pgf';
 
 	var fileStr =
 		'// Type definitions for FrameMaker ' +
@@ -19,7 +19,7 @@
 	// ==================================================
 
 	var doc = app.ActiveDoc;
-	var targetObj = doc.ElementSelection.beg.parent;
+	var targetObj = doc.TextSelection.beg.obj;
 
 	// ==================================================
 
