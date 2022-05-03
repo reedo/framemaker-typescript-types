4ad29e944d8ae423ca347d8b83cc72f5429d94a0
dan.reed <dan.reed@vistair.com>
2022-05-04 00:01:21 +0100

    Done

--- makeTypedefFile.js
+++ makeTypedefFile.js
@@ -2,7 +2,7 @@
 	var BASE_DIR =
 		'C:/Vistair/FMConfig/FM16/ExtendScripts/types/framemaker-typescript-types/f_api/objects/';
 
-	var objectName = 'FilterArgs';
+	var objectName = 'PgfFmt';
 
 	var fileStr =
 		'// Type definitions for FrameMaker ' +
@@ -19,7 +19,7 @@
 	// ==================================================
 
 	var doc = app.ActiveDoc;
-	var targetObj = true;
+	var targetObj = doc.FirstPgfFmtInDoc;
 
 	// ==================================================
 
