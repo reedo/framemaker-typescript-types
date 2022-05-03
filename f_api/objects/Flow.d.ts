3116950a9fbcfe205fa34349381be92f4a18f732
dan.reed <dan.reed@vistair.com>
2022-05-03 01:54:08 +0100

    Done

---
+++ f_api/objects/Flow.d.ts
@@ -0,0 +1,42 @@
+// Type definitions for FrameMaker Flow object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface Flow extends FMNamedObject {
+	Direction: number;
+
+	readonly FirstTextFrameInFlow: TextFrame;
+
+	FlowIsAutoConnect: boolean;
+
+	FlowIsFeathered: boolean;
+
+	FlowIsPostScript: boolean;
+
+	FlowIsSynchronized: boolean;
+
+	readonly HighestLevelElement: Element;
+
+	readonly LastTextFrameInFlow: TextFrame;
+
+	MaxInterPgfPadding: number;
+
+	MaxInterlinePadding: number;
+
+	MinHang: number;
+
+	readonly NextFlowInDoc: Flow;
+
+	readonly ResolvedDirection: number;
+
+	SideHeadRoomInFlow: boolean;
+
+	Spacing: number;
+
+	UserString: string;
+
+	Delete(): FrameErrorCode;
+
+	GetText(flags: number): TextItems;
+
+	GetText2(flags: number, flags2: number): TextItems;
+}
