f3d41823b5e31bfddedaec795536d3055f6fb981
ReedError <info@reederror.com>
2022-05-03 23:17:36 +0100

    Done (#151)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

---
+++ f_api/objects/Command.d.ts
@@ -0,0 +1,56 @@
+// Type definitions for FrameMaker Command object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface Command extends FMNamedObject {
+	CanHaveCheckMark: boolean;
+
+	CheckMarkIsOn: boolean;
+
+	CommandNum: number;
+
+	EnabledWhen: number;
+
+	readonly ExpandOMaticParent: Command;
+
+	readonly Fcode: number;
+
+	readonly Fcodes: UInts;
+
+	HasShiftOrUnshiftCommand: number;
+
+	HelpLink: string;
+
+	KeyboardShortcutLabel: string;
+
+	KeyboardShortcuts: Strings;
+
+	Label: string;
+
+	Labels: Strings;
+
+	readonly MenuItemIsEnabled: boolean;
+
+	readonly MenuItemType: number;
+
+	readonly Mode: number;
+
+	readonly NextCommandInSession: Command;
+
+	NextMenuItemInMenu: Command | Menu | MenuItemSeparator;
+
+	readonly NextMenuItemInSession: Command | Menu | MenuItemSeparator;
+
+	ParentMenuId: InvalidObject;
+
+	PrevMenuItemInMenu: Command | Menu | MenuItemSeparator;
+
+	Product: number;
+
+	ShiftOrUnshiftCommand: Command;
+
+	View: number;
+
+	Window: number;
+
+	Delete(): FrameErrorCode;
+}
