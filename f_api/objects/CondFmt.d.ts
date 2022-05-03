7efdde02bb92767cd6a5ed874b60f6e1ba9f4e5f
dan.reed <dan.reed@vistair.com>
2022-05-03 02:42:43 +0100

    Done

---
+++ f_api/objects/CondFmt.d.ts
@@ -0,0 +1,24 @@
+// Type definitions for FrameMaker CondFmt object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface CondFmt extends FMNamedObject {
+	BkColor: Color;
+
+	CondFmtIsShown: boolean;
+
+	readonly NextCondFmtInDoc: CondFmt;
+
+	SepOverride: Color;
+
+	StyleOverride: number;
+
+	UseBkColor: boolean;
+
+	UseSepOverride: boolean;
+
+	UserString: string;
+
+	Delete(): FrameErrorCode;
+
+	DeleteCondTag(...args: any): unknown;
+}
