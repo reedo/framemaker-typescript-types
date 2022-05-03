190f67bac38da33455e5ad59e8d0f1d5d12b6329
ReedError <info@reederror.com>
2022-05-04 00:15:26 +0100

    Done (#158)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

---
+++ f_api/objects/Ellipse.d.ts
@@ -0,0 +1,6 @@
+// Type definitions for FrameMaker Ellipse object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface Ellipse extends FMGraphic {
+	readonly RectangleIsSmoothed: boolean;
+}
