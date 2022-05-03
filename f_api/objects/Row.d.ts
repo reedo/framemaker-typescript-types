7c8035bfd32c6ef4b1cab206ccb011599255faca
dan.reed <dan.reed@vistair.com>
2022-05-03 02:48:55 +0100

    Done

---
+++ f_api/objects/Row.d.ts
@@ -0,0 +1,52 @@
+// Type definitions for FrameMaker Row object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface Row extends FMObject {
+	CondFmtIsShown: boolean;
+
+	readonly Element: Element;
+
+	readonly FirstCellInRow: Cell;
+
+	readonly Height: number;
+
+	InCond: FMObjectArray<CondFmt>;
+
+	readonly LocX: number;
+
+	readonly LocY: number;
+
+	readonly NextRowInTbl: Row;
+
+	readonly PrevRowInTbl: Row;
+
+	readonly RowIsShown: boolean;
+
+	RowKeepWithNext: boolean;
+
+	RowKeepWithPrev: boolean;
+
+	RowMaxHeight: number;
+
+	RowMinHeight: number;
+
+	RowStart: number;
+
+	readonly RowTbl: Tbl;
+
+	readonly RowType: number;
+
+	SepOverride: Color;
+
+	StyleOverrides: number;
+
+	UseSepOverride: boolean;
+
+	UserString: string;
+
+	readonly Width: number;
+
+	AddRows(direction: number, numNewRows: number): FrameErrorCode;
+
+	Delete(): FrameErrorCode;
+}
