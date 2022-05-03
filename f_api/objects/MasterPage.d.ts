f16c77ec4f50cca7633fd0da121259241061c461
dan.reed <dan.reed@vistair.com>
2022-05-04 00:36:19 +0100

    Done

---
+++ f_api/objects/MasterPage.d.ts
@@ -0,0 +1,20 @@
+// Type definitions for FrameMaker MasterPage object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface MasterPage extends FMNamedObject {
+	readonly PageFrame: UnanchoredFrame;
+
+	readonly PageHeight: number;
+
+	readonly PageNext: MasterPage;
+
+	readonly PageNum: number;
+
+	readonly PagePrev: MasterPage;
+
+	readonly PageWidth: number;
+
+	ApplyPageLayout(srcPage: Page): FrameErrorCode;
+
+	Delete(): FrameErrorCode;
+}
