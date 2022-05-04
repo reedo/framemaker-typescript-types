41967a2bb654b7245fee019d213d4984e3aadcf0
ReedError <info@reederror.com>
2022-05-04 02:46:57 +0100

    Done (#179)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

---
+++ f_api/objects/VarFmt.d.ts
@@ -0,0 +1,12 @@
+// Type definitions for FrameMaker VarFmt object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface VarFmt extends FMNamedObject {
+	Fmt: string;
+
+	readonly NextVarFmtInDoc: VarFmt;
+
+	readonly SystemVar: number;
+
+	Delete(): FrameErrorCode;
+}
