d99e4300d6567f7f9b063625a8408e7807ed0c8f
dan.reed <dan.reed@vistair.com>
2022-05-04 03:42:03 +0100

    Done

---
+++ f_api/models/FMTiObject.d.ts
@@ -0,0 +1,37 @@
+// Type definitions for generic properties of FrameMaker Ti objects
+// Definitions by Dan Reed <https://danreed.dev>
+
+/** A FrameMaker text inset object. */
+interface FMTiObject extends FMUniqueObject {
+	ImportHint: string;
+
+	LastUpdate: number;
+
+	Name: string;
+
+	readonly NextTiInDoc: Ti;
+
+	readonly TextRange: TextRange;
+
+	TiAutomaticUpdate: boolean;
+
+	TiFile: string;
+
+	TiFileModDate: object;
+
+	TiLocked: boolean;
+
+	ConvertToText(): void;
+
+	Delete(): FrameErrorCode;
+
+	DeletePropByName(propName: string): FrameErrorCode;
+
+	DeleteTextInsetContents(): FrameErrorCode;
+
+	GetText(flags: number): TextItems;
+
+	GetText2(flags: number, flags2: number): TextItems;
+
+	UpdateTextInset(): FrameErrorCode;
+}
--- f_api/objects/TiText.d.ts
+++ f_api/objects/TiText.d.ts
@@ -1,40 +1,8 @@
 // Type definitions for FrameMaker TiText object
 // Definitions by Dan Reed <https://danreed.dev>
 
-interface TiText extends FMUniqueObject {
+interface TiText extends FMTiObject {
 	TiEOLisEOP: boolean;
 
 	TiTextEncoding: string;
-
-	ImportHint: string;
-
-	TiLocked: boolean;
-
-	Name: string;
-
-	NextTiInDoc: Ti;
-
-	TextRange: TextRange;
-
-	TiAutomaticUpdate: boolean;
-
-	TiFile: string;
-
-	TiFileModDate: string;
-
-	LastUpdate: number;
-
-	ConvertToText(): void;
-
-	Delete(): FrameErrorCode;
-
-	DeletePropByName(propName: string): FrameErrorCode;
-
-	DeleteTextInsetContents(): FrameErrorCode;
-
-	GetText(flags: number): TextItems;
-
-	GetText2(flags: number, flags2: number): TextItems;
-
-	UpdateTextInset(): FrameErrorCode;
 }
