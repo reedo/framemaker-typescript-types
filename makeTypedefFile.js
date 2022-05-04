1a96d3dfd1baccbcf2622e20ce1cd8c1d0ec6a2a
dan.reed <dan.reed@vistair.com>
2022-05-04 03:13:36 +0100

    Done

--- makeTypedefFile.js
+++ makeTypedefFile.js
@@ -2,7 +2,7 @@
 	var BASE_DIR =
 		'C:/Vistair/FMConfig/FM16/ExtendScripts/types/framemaker-typescript-types/f_api/objects/';
 
-	var objectName = 'TblFmt';
+	var objectName = 'TiApiClient';
 
 	var fileStr =
 		'// Type definitions for FrameMaker ' +
@@ -19,7 +19,7 @@
 	// ==================================================
 
 	var doc = app.ActiveDoc;
-	var targetObj = doc.FirstTblFmtInDoc;
+	var targetObj = doc.NewAnchoredTiApiClient(doc.TextSelection.beg);
 
 	// ==================================================
 
