f058fcc204b4063829a7b204fe45361f9853f9c3
ReedError <info@reederror.com>
2022-05-04 01:36:53 +0100

    Done (#165)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

---
+++ f_api/objects/InlineComponent.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for FrameMaker InlineComponent object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface InlineComponent extends FMUniqueObject {
+	ExtractTags: Strings;
+
+	readonly InlineComponentType: FV_MiniTOC;
+
+	InsertLinks: boolean;
+
+	readonly NextInlineComponentInDoc: InlineComponent;
+
+	readonly TextRange: TextRange;
+
+	Delete(): FrameErrorCode;
+}
