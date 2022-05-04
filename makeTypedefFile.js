340998d9f3044c05fa0c6c408bcb0d77c9a09ecb
ReedError <info@reederror.com>
2022-05-04 03:28:48 +0100

    Done (#184)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

--- makeTypedefFile.js
+++ makeTypedefFile.js
@@ -2,7 +2,7 @@
 	var BASE_DIR =
 		'C:/Vistair/FMConfig/FM16/ExtendScripts/types/framemaker-typescript-types/f_api/objects/';
 
-	var objectName = 'TiText';
+	var objectName = 'TiFlow';
 
 	var fileStr =
 		'// Type definitions for FrameMaker ' +
