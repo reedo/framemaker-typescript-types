b1db6a8ced2151160efab960496eddaed6408ad1
ReedError <info@reederror.com>
2022-05-04 02:49:21 +0100

    Done (#180)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

---
+++ f_api/objects/Var.d.ts
@@ -0,0 +1,22 @@
+// Type definitions for FrameMaker Var object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface Var extends FMUniqueObject {
+	readonly Element: Element;
+
+	Locked: boolean;
+
+	readonly NextVarInDoc: Var;
+
+	readonly TextRange: TextRange;
+
+	UserString: string;
+
+	VarFmt: VarFmt;
+
+	Delete(): FrameErrorCode;
+
+	GetText(flags: number): TextItems;
+
+	GetText2(flags: number, flags2: number): TextItems;
+}
