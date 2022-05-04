1ad0f1da48f804bc41372f66c42a506d77f0a3c8
dan.reed <dan.reed@vistair.com>
2022-05-04 02:04:29 +0100

    Done

---
+++ f_api/objects/Polyline.d.ts
@@ -0,0 +1,10 @@
+// Type definitions for FrameMaker Polyline object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface Polyline extends FMGraphic {
+	NumPoints: number;
+
+	Points: Points;
+
+	PolyIsBezier: boolean;
+}
