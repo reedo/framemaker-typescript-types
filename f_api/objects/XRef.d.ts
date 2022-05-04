bc699e3a6f9657261c3c5cfd53ae11650d1b909d
ReedError <info@reederror.com>
2022-05-04 02:44:50 +0100

    Done (#178)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

---
+++ f_api/objects/XRef.d.ts
@@ -0,0 +1,38 @@
+// Type definitions for FrameMaker XRef object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface XRef extends FMUniqueObject {
+	readonly Element: Element;
+
+	Locked: boolean;
+
+	readonly NextXRefInDoc: XRef;
+
+	readonly TextRange: TextRange;
+
+	UserString: string;
+
+	XRefAltText: string;
+
+	XRefClientName: string;
+
+	XRefClientType: string;
+
+	XRefFile: string;
+
+	XRefFmt: XRefFmt;
+
+	readonly XRefIsUnresolved: boolean;
+
+	XRefSrcElemNonUniqueId: string;
+
+	XRefSrcIsElem: boolean;
+
+	XRefSrcText: string;
+
+	Delete(): FrameErrorCode;
+
+	GetText(flags: number): TextItems;
+
+	GetText2(flags: number, flags2: number): TextItems;
+}
