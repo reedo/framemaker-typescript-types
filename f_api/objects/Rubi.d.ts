174d484ecd582fc030ba3aac66551920eaf609b5
ReedError <info@reederror.com>
2022-05-04 02:37:37 +0100

    Done (#176)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

---
+++ f_api/objects/Rubi.d.ts
@@ -0,0 +1,18 @@
+// Type definitions for FrameMaker Rubi object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface Rubi extends FMUniqueObject {
+	readonly Element: Element;
+
+	readonly NextRubiInDoc: Rubi;
+
+	readonly OyamojiTextRange: TextRange;
+
+	readonly RubiElement: Element;
+
+	readonly RubiTextRange: TextRange;
+
+	Delete(): FrameErrorCode;
+
+	GetText2(flags: number, flags2: number): TextItems;
+}
