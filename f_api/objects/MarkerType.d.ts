eeb50cda5ad4b265877e8d7703cce5b79b5bf4ee
ReedError <info@reederror.com>
2022-05-04 02:20:54 +0100

    Done (#172)
    Co-authored-by: dan.reed <dan.reed@vistair.com>

---
+++ f_api/objects/MarkerType.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for FrameMaker MarkerType object
+// Definitions by Dan Reed <https://danreed.dev>
+
+interface MarkerType extends FMNamedObject {
+	InvariantName: string;
+
+	NextMarkerTypeInDoc: MarkerType;
+
+	Public: boolean;
+
+	readonly Required: boolean;
+
+	Transient: boolean;
+
+	Delete(): FrameErrorCode;
+}
