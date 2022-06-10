f5131987f9d08bafbe9c069feffff10450b8ee9f
dan.reed <dan.reed@vistair.com>
2022-06-10 17:50:31 +0100

    Add missing props

--- f_api/data-structures/TypedVal.d.ts
+++ f_api/data-structures/TypedVal.d.ts
@@ -3,68 +3,76 @@
 
 /** Specifies an individual property value. */
 interface TypedVal {
-	/** Indicates the type of value the structure provides. */
-	valType: number;
+  /** Indicates the type of value the structure provides. */
+  valType: number;
 
-	/** An integer. */
-	ival: number;
+  /** An integer. */
+  ival: number;
 
-	/** String value. */
-	sval: string;
+  /** String value. */
+  sval: string;
 
-	/** Set of metrics. */
-	msval: Metrics;
+  /** Set of metrics. */
+  msval: Metrics;
 
-	/** Set of strings. */
-	ssval: Strings;
+  /** Set of strings. */
+  ssval: Strings;
 
-	/** Set of points. */
-	psval: Points;
+  /** Set of points. */
+  psval: Points;
 
-	/** Set of tabs. */
-	tsval: Tabs;
+  /** Set of tabs. */
+  tsval: Tabs;
 
-	/** Text location. */
-	tlval: TextLoc;
+  /** Text location. */
+  tlval: TextLoc;
 
-	/** Text range. */
-	trval: TextRange;
+  /** Text range. */
+  trval: TextRange;
 
-	/** Element Catalog values. */
+  /** Element Catalog values. */
 
-	csval: ElementCatalogEntries;
+  csval: ElementCatalogEntries;
 
-	/** Set of integers. */
-	isval: Ints;
+  /** Set of integers. */
+  isval: Ints;
 
-	/** Set of unsigned integers. */
-	uisval: UInts;
+  /** Set of unsigned integers. */
+  uisval: UInts;
 
-	/** Set of attribute definitions. */
-	adsval: AttributeDefs;
+  /** Set of attribute definitions. */
+  adsval: AttributeDefs;
 
-	/** Set of attribute values. */
-	asval: Attributes;
+  /** Set of attribute values. */
+  asval: Attributes;
+
+  /** Set of typed vals. */
+  valsval: TypedVals;
+
+  /** A FrameMaker object. */
+  obj: FMObject;
 }
 
 interface TypedValConstructor {
-	new (): TypedVal;
-	new (
-		item:
-			| number
-			| string
-			| Metrics
-			| Strings
-			| Points
-			| Tabs
-			| TextLoc
-			| TextRange
-			| ElementCatalogEntries
-			| Ints
-			| UInts
-			| AttributeDefs
-			| Attributes
-	): TypedVal;
+  new (): TypedVal;
+  new (
+    item:
+      | number
+      | string
+      | Metrics
+      | Strings
+      | Points
+      | Tabs
+      | TextLoc
+      | TextRange
+      | ElementCatalogEntries
+      | Ints
+      | UInts
+      | AttributeDefs
+      | Attributes
+      | TypedVals
+      | FMObject
+  ): TypedVal;
 }
 
 declare const TypedVal: TypedValConstructor;
